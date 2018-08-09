2624a4a3b90694cc95061144744bd567419d8aac
lorielewis300 <42074691+lorielewis300@users.noreply.github.com>
2018-08-09 09:07:36 -0400

    Comment about CreateDate field.

--- MCC_AZ_Qtrly_Cred_CreateDate.sql
+++ MCC_AZ_Qtrly_Cred_CreateDate.sql
@@ -1,6 +1,8 @@
 
 /*Fields in the report that we will likely need to provider to networking for newly credentialed providers.
 
+Note: The CreateDate has not been added to the QNXT extract yet, but it has been requested. 
+
 SHORTEST TIME TO LOAD PROVIDER ID IN
 CLAIMS SYSTEM4	LONGEST TIME TO LOAD PROVIDER ID IN
 CLAIMS SYSTEM4	AVERAGE LENGTH OF TIME TO LOAD PROVIDER ID INTO CLAIMS SYSTEM4
